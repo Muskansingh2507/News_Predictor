8cde41b5ed103e578e6f629bb5fb258a4dc5d662
Muskan Singh <muskansingh212019@gmail.com>
2025-09-18 06:04:33 +0530

    Update untitled12_ (1).py

--- untitled12_ (1).py
+++ untitled12_ (1).py
@@ -9,7 +9,6 @@ Original file is located at
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
